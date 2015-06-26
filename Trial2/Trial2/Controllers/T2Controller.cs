a39748405dd28e8e00c039240f52523f5fe2d6d0
juliepromact <julie@promactinfo.com>
2015-06-26 13:44:19 +0530

    Trial2 added

---
+++ Trial2/Trial2/Controllers/T2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trial2.Controllers
+{
+    public class T2Controller : Controller
+    {
+        //
+        // GET: /T2/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
